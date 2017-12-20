9b1aad33eab3f2dbd58dfd425ea84d18bfb51765
flyspot <webspot84@gmail.com>
2017-12-20 23:27:12 +0200

    Project deploy on GitHub

---
+++ src/main/java/lesson10_collections/ArrayListTest.java
@@ -0,0 +1,28 @@
+package lesson10_collections;
+
+/**
+ * Created by FlySpot on 14.12.2017.
+ */
+import java.util.*;
+import java.io.*;
+
+public class ArrayListTest {
+    ArrayList lst = new ArrayList();
+    Random generator = new Random();
+
+    void addRandom() {
+        lst.add(new Integer(generator.nextInt()));
+    }
+
+
+    public String toString() {
+        return lst.toString();
+    }
+
+    public static void main(String args[]) {
+        ArrayListTest tst = new ArrayListTest();
+        for(int i = 0; i < 100; i++ )
+            tst.addRandom();
+        System.out.println("Сто случайных чисел: "+tst.toString());
+    }
+}
